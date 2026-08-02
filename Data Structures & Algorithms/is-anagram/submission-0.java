cde1164c13503ab480ae27be3c62e6d65305e40f
Nihal1203 <118458326+Nihal1203@users.noreply.github.com>
2026-08-02 14:32:47 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> m1 = new HashMap<>();
+        Map<Character, Integer> m2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            m1.put(ch, m1.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            m2.put(ch, m2.getOrDefault(ch, 0) + 1);
+        }
+
+        return m1.equals(m2);
+    }
+}
